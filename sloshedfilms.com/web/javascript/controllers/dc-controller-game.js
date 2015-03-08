edbb465d64995008fac6071bb19dfae56f23f272
Anthony D'Agostino <tj.dagostino@gmail.com>
2015-03-08 13:17:52 -0700

    fixing game to read page json

--- sloshedfilms.com/web/javascript/controllers/dc-controller-game.js
+++ sloshedfilms.com/web/javascript/controllers/dc-controller-game.js
@@ -3,13 +3,8 @@ var name = "controller.game";
 
     var controller = function(){
         this.init = function(){
-            this.game = $dc.utils.getJSON('gameJSON','dc-game-json');
-            var vo = $dc.utils.getJSON('vendorJSON','dc-vendor-json');
-            var va = [];
-            for (var i in vo){
-                va[parseInt(i)] = vo[i];
-            }
-            this.vendors = va;
+            this.page = $dc.utils.getJSON('pageJSON','dc-page-json');
+            this.game = this.page.game;
             this.cdn = $dc.globals.cdn;
         }
     };
