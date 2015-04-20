6d0213607ba7716cf501f4e6ea9ffe0d4c986238
Anthony D'Agostino <tj.dagostino@gmail.com>
2015-04-20 15:59:54 -0700

    adding minified files

--- drinking_cinema_back_end/application/models/globals.php
+++ drinking_cinema_back_end/application/models/globals.php
@@ -4,7 +4,7 @@
         private $game_images_directory = "Games/";
         private $upload_images_directory = "uli/";
         private $images_directory = "Images/";
-        private $script_guid = "1429569881812_BhX2Xa50B9WSwFEY";
+        private $script_guid = "1429570576195_Msja1JVj6FftwgXK";
 
         function __construct() {
             // Call the Model constructor
